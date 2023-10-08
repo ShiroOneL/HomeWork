c380100d9da9f094325128ec804593e61e202e1e
Даниил Авилов <avilovdanya11@gmail.com>
2023-10-08 20:31:17 +0300

    Update Hello.pas

--- hello.pas
+++ hello.pas
@@ -1,3 +1,12 @@
 ﻿begin
-  
+  println('1.Русский');
+  println('2.Английский');
+  println('3.Немецкий');
+  var a := readinteger('Выберете язык(1-3):');
+  assert(a in 1..3);
+  case a of
+    1: print('Привет, мир!');
+    2: print('Hello World!');
+    3: print('Hallo Welt!');
+  end;
 end.
